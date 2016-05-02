04b48ef893d1df91b1bf9346c9b8239c1a9d6a0e
MercuryHunter <ujonathanalp@gmail.com>
2016-05-02 12:45:44 +0200

    Implemented shell command execution using subprocess, and set up the commands for cpu and memory monitoring

--- webserver.py
+++ webserver.py
@@ -1,5 +1,22 @@
+import subprocess
+
+cpu_command = ["top", "-o", "cpu", "-l", "2", "-n", "10"]
+mem_command = ["top", "-o", "mem", "-l", "2", "-n", "10"]
+
 def main():
-	print("Stuff")
+	cpu_output = get_command_output(cpu_command)
+	for line in cpu_output:
+		print(line)
+	#print(cpu_output)
+
+def cpu_graph():
+	cpu_output = get_command_output(cpu_command)
+
+def mem_graph():
+	mem_output = get_command_output(mem_command)
+
+def get_command_output(command):
+	return str(subprocess.check_output(command)).split("\\n")
 
 if __name__ == '__main__':
 	main()
