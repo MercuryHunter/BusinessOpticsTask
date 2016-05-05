d70162374a0ca7af8c099fc120154e30998226ba
Ural Jonathan Alp <alpura001@sl-dual-140.cs.uct.ac.za>
2016-05-05 15:55:24 +0200

    Fixed up information getting and the web-interface

--- src/webserver.py
+++ src/webserver.py
@@ -8,9 +8,9 @@ def main():
 	main_info = sys_info_ubuntu.general_info()
 	main_cpu_info = main_info[0]
 	main_mem_info = main_info[1]
-
-	cpu_ten = [] #sys_info_ubuntu.cpu_top_ten()
-	mem_ten = [] #sys_info_ubuntu.mem_top_ten()
+	
+	cpu_ten = sys_info_ubuntu.cpu_top_ten()
+	mem_ten = sys_info_ubuntu.mem_top_ten()
 
 	return render_template("index.html", CPU = main_cpu_info, MEM = main_mem_info, CPU_TEN=cpu_ten, MEM_TEN=mem_ten)
 
